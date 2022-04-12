82d8c6f008b20e0426ea65914ab6313a9b654c32
AJ-Binks <98901979+AJ-Binks@users.noreply.github.com>
2022-04-12 16:44:48 -0500

    Create mle.R

---
+++ Exam/R/mle.R
@@ -0,0 +1,30 @@
+#' MLE function
+#'
+#' Calculates the maximum likelihood estimator for lambda. Used for object of class \code{PoisMLE}.
+#'
+#' @param y The vector of observed data
+#'
+#' @return Numeric value
+#'  \item{MLE}{The maximum likelihood estimator for lamda}
+#' @author Annie Jarman
+#' @note This is used internally for the \code{estimatePois} function
+#' @aliases MLE
+#' @examples
+#' 
+#' set.seed(1227)
+#' y <- rpois(12270,130)
+#' mle(y)
+#' @seealso \code{\link{estimatePois}}
+#' @seealso \code{\link{logLik}}
+#' @seealso \code{\link{standardError}}
+#' @import methods
+#' @import stats
+#' @rdname mle
+#' @export
+
+#write mle function - dont need to use S4 (devtools lecture)
+mle <- function(y){
+  n <- length(y)
+  MLE <- sum(y, na.rm=TRUE)/n
+  return(MLE)
+}
