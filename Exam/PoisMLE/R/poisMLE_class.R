7cfd2d3891c7f7330087da541474fbc7834699fe
AJ-Binks <a.a.jarman@wustl.edu>
2022-04-12 20:34:27 -0500

    revert

--- Exam/PoisMLE/R/poisMLE_class.R
+++ Exam/PoisMLE/R/poisMLE_class.R
@@ -80,37 +80,3 @@ setMethod("initialize", "PoisMLE",
           }
 ) 
 
-#plot
-#set generic
-setGeneric(f="plot",
-           signature(x="PoisMLE")
-           definition=function(x){
-             plot(x@MLE)
-           }
-)
-
-#set method
-setMethod(f="plot",
-          signature(x="PoisMLE"),
-          definition=function(x){
-            #make sequence of lambdas around mle
-            lambdaSeq <- seq(x@MLE - 2, x@MLE + 2, by=0.1)
-            #take log likelihood of lambda sequence
-            likelihoods <- logLik(x@y, lambdaSeq)
-            #put together in dat
-            dat <- data.fame("x"=lambdaSeq, "y"=likelihoods)
-            #create df
-            df <- data.frame("x"=x@MLE, "y"=x@LL)
-            #use ggplot2 to plot
-            plot <- ggplot2::ggplot()+ 
-              geom_vline(xintercept=x@MLE, color="red")+ #MLE
-              geom_point(aes(x=x, y=y), data=df, color="red")+ #ME
-              geom_point(aes(x=lambdaSeq, y=likelihoods), data=dat) + #points for potential lambdas and their likelihood
-              geom_vline(aes(xintercept=lambdaHat, color="red"))+ #vertical line for MLE
-              geom_vline(aes(xintercept=x@MLE + 1.96*se, linetype="dashed", color="grey")) + #vertical line for higher confidence interval
-              geom_vline(aes(xintercept=x@MLE - 1.96*se, linetype="dashed", color="grey")) + #vertical line for lower confidence interval
-              labs(x="Lambda", y="Log Likelihood") +
-              theme_minimal()
-            return(plot)  
-          }
-)
